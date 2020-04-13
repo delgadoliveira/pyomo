a71a1d10b16c07ab4cfc0c8ea9d8f3796d44a729
delgadoliveira <delgado.oliveira@gmail.com>
2020-04-13 13:25:10 -0300

    Solving models through Pyomo

---
+++ abstract3.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from pyomo.environ import *
+
+model = ConcreteModel()
+
+# declare decision variables
+model.x = Var(domain=NonNegativeReals)
+
+# declare objective
+model.profit = Objective(
+    expr = 40*model.x,
+    sense = maximize)
+
+# declare constraints
+model.demand = Constraint(expr = model.x <= 40)
+model.laborA = Constraint(expr = model.x <= 80)
+model.laborB = Constraint(expr = 2*model.x <= 100)
+
+print("Profit = ", model.profit(), " per week")
+print("X = ", model.x(), " units per week")
